9cd95a5718fab96f13532927a5a295c7faa9bdae
ben0128 <99779906+ben0128@users.noreply.github.com>
2026-05-02 17:55:05 +0800

    Add: decode-string - submission-2

---
+++ Data Structures & Algorithms/decode-string/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        n = len(s)
+        def recursive(idx):
+            num = 0
+            word = []
+            # for k, c in enumerate(s[idx:], idx):
+            while idx < n:
+                c = s[idx]
+                if c.isdigit():
+                    num = num*10 + int(c)
+                elif c == '[':
+                    tmpWords, nxtIdx = recursive(idx+1)
+                    word.append(''.join(tmpWords)*num)
+                    idx, num = nxtIdx, 0
+                elif c == ']':
+                    return [word, idx]
+                else:
+                    word.append(c)
+                idx += 1
+            return word
+        return ''.join(recursive(0))
