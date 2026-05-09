e04127eafb76fa9949e0aede2bc1f951e950dddc
ben0128 <99779906+ben0128@users.noreply.github.com>
2026-05-09 23:50:01 +0800

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        visited = defaultdict(int)
+        visited[0] = 1
+        n = len(nums)
+        preflix = [0] * (n+1)
+        
+        for i in range(n):
+            preflix[i+1] = preflix[i]+nums[i]
+        count = 0
+        
+        for i in range(n):
+            count += visited[preflix[i+1] - k]
+            visited[preflix[i+1]] += 1
+        return count
