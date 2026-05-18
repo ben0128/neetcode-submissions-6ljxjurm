8087122cc63757debaa67d3a2bbee67ff4e64e25
ben0128 <99779906+ben0128@users.noreply.github.com>
2026-05-19 00:03:44 +0800

    Add: house-robber - submission-16

---
+++ Data Structures & Algorithms/house-robber/submission-16.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # 1 <= nums <= 100
+        # 0 <= nums[i] <= 100
+        # dp[i] => 一定要搶index i 的房子
+        n = len(nums)
+        if n <= 1:
+            return nums[0]
+        # dp = [0]*n
+        prev1, prev2 = nums[0], max(nums[0], nums[1])
+        # 4, 4,10,10,17
+        for i in range(2, n):
+            curr = max(prev1+nums[i], prev2)
+            prev1 = prev2
+            prev2 = curr
+        
+        return prev2
+    # nums = [1] => 1
+    # nums = [1, 2] => 2
+    # nums = [4, 2, 1] => 5
+    # nums = [4, 2, 6, 3, 7] => 17
+    # dp   = [4, 4,10,10,17]
+    # print(rob([1]) == 1)
+    # print(rob([1,2]) == 2)
+    # print(rob([4,2,1]) == 5)
+    # print(rob([4,2,6,3,7]) == 17)
